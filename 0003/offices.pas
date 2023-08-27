f552553af8be3cab45342a13f3a4334b86e58be6
duzenko <anton.duzenko@gmail.com>
2023-08-27 19:19:47 +0300

    0003 small optimization

--- 0003/offices.pas
+++ 0003/offices.pas
@@ -1,28 +1,68 @@
 program offices;
 {$mode objfpc}
 
-uses SysUtils, Math, Classes;
+uses SysUtils;
 
-function CompareCompany(Item1, Item2: Pointer): Integer;
+function Compare(Item1, Item2: Integer): Integer; inline;
 begin
-  	Result := NativeUInt(Item1) and $3fff - NativeUInt(Item2) and $3fff;
+  	Result := Item1 - Item2;
 end;
 
 var
-   i, x: Integer;
-   Companies: TList;
+   CompanySizes: array[0..100000] of Integer;
+   CompanyIndex: array[0..100000] of Integer;
+
+Procedure QuickSort(L, R : Longint);
+var
+  I, J, Qi : Longint;
+  P, Q : Integer;
 begin
-	Companies := TList.Create;
-	readln(x);
-   Companies.Count := x;
-   for i := 0 to Companies.Count-1 do begin
-      read(x);
-      Companies[i] := Pointer(i shl 14 + x);
+ repeat
+   I := L;
+   J := R;
+   P := CompanySizes[ (L + R) div 2 ];
+   repeat
+     while Compare(P, CompanySizes[i]) > 0 do
+       I := I + 1;
+     while Compare(P, CompanySizes[J]) < 0 do
+       J := J - 1;
+     If I <= J then
+     begin
+       Q := CompanySizes[I];
+       CompanySizes[I] := CompanySizes[J];
+       CompanySizes[J] := Q;
+       Qi := CompanyIndex[I];
+       CompanyIndex[I] := CompanyIndex[J];
+       CompanyIndex[J] := Qi;
+       I := I + 1;
+       J := J - 1;
+     end;
+   until I > J;
+   if J - L < R - I then
+   begin
+     if L < J then
+       QuickSort(L, J);
+     L := I;
+   end
+   else
+   begin
+     if I < R then
+       QuickSort(I, R);
+     R := J;
    end;
-   Companies.Sort(@CompareCompany);
-   for i := 0 to Companies.Count-1 do begin
-      x := NativeUInt(Companies[i]) shr 14 + 1;
-   	Write(x, ' ');
+ until L >= R;
+end;
+
+var
+   i, n: LongWord;
+begin
+	readln(n);
+   for i := 0 to n-1 do begin
+      CompanyIndex[i] := i;
+      read(CompanySizes[i]);
    end;
+   QuickSort(0, n-1);
+   for i := 0 to n-1 do
+   	Write(CompanyIndex[i] + 1, ' ');
 end.
 
